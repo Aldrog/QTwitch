f089e0bcdbdbb8081e78195c724b75d9b41ce2f5
Andrew Penkrat <contact.aldrog@gmail.com>
2019-03-07 00:51:03 +0300

    Add self property to Models::UserInfo
    Setting it to true will get user by Bearer token.

--- Models/userinfo.cpp
+++ Models/userinfo.cpp
@@ -55,3 +55,29 @@ void UserInfo::setLogin(const QString &newLogin)
     });
     Client::getClient()->send(request);
 }
+
+void UserInfo::setSelf(bool newSelf)
+{
+    if (newSelf == mSelf)
+        return;
+    mSelf = newSelf;
+    emit selfChanged(mSelf);
+    if (!mSelf)
+        return;
+
+    auto request = std::make_shared<Helix::UsersRequest>();
+    connect(request.get(), &Request::responseReceived,
+            [this, request] (const std::shared_ptr<Response> &response)
+    {
+        auto data = std::unique_ptr<Helix::UsersList>(static_cast<Helix::UsersList*>(response->object.release()));
+        if (!data->data.empty()) {
+            mUserId = data->data.front().id;
+            emit userIdChanged(mUserId);
+            mLogin = data->data.front().login;
+            emit loginChanged(mLogin);
+            mDisplay = data->data.front().displayName;
+            emit displayChanged(mDisplay);
+        }
+    });
+    Client::getClient()->send(request);
+}
--- Models/userinfo.h
+++ Models/userinfo.h
@@ -12,26 +12,31 @@ class QTWITCHSHARED_EXPORT UserInfo : public QObject
     Q_OBJECT
     Q_PROPERTY(QString userId READ userId WRITE setUserId NOTIFY userIdChanged)
     Q_PROPERTY(QString login READ login WRITE setLogin NOTIFY loginChanged)
+    Q_PROPERTY(bool self READ self WRITE setSelf NOTIFY selfChanged)
     Q_PROPERTY(QString display READ display NOTIFY displayChanged)
 public:
     explicit UserInfo(QObject *parent = nullptr);
 
     inline QString userId() const { return mUserId; }
     inline QString login() const { return mLogin; }
+    inline bool self() const { return mSelf; }
     inline QString display() const { return mDisplay; }
 
     void setUserId(const QString &newUserId);
     void setLogin(const QString &newLogin);
+    void setSelf(bool newSelf);
 
 signals:
     void userIdChanged(const QString &newUserId);
     void loginChanged(const QString &newLogin);
+    void selfChanged(bool newSelf);
     void displayChanged(const QString &newDisplay);
 
 private:
     QString mUserId;
     QString mLogin;
     QString mDisplay;
+    bool mSelf = false;
 };
 
 }
